bcbe315fbce21d9b777f336f57808bd7b8741835
Corvimae <may@maybreak.com>
2020-07-16 12:55:09 -0500

    Initial commit.

---
+++ modules/selfTokenDelete.js
@@ -0,0 +1,21 @@
+Hooks.on('ready', () => {
+  document.body.addEventListener('keydown', event => {
+    if (event.key === 'Backspace') {
+      game.socket.emit('module.self-token-delete', {
+        event: 'request-deletion',
+        tokens: canvas.tokens.controlled.filter(token => token.owner).map(token => ({
+          id: token.id,
+          sceneId: token.scene.id,
+        })),
+      });
+    }
+  });
+
+  game.socket.on('module.self-token-delete', async data => {
+    if (data.event === 'request-deletion' && game.user.isGM) {
+      await Promise.all(data.tokens.map(async ({ id, sceneId }) => {
+        const scene = game.scenes.get(sceneId).deleteEmbeddedEntity("Token", id);
+      }));
+    }
+  });
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Self Token Delete for Foundry VTT
+
+Plugin for Foundry VTT that allows users to delete tokens they own. Select a token and hit Backspace to remove it from the scene.
+
+A GM must be in game for the token to get deleted.
+
+**This module is inherently insecure, and opens an exploit that can allow malicious users to delete tokens they do not own. Do not use
+this module with players you do not trust.**
